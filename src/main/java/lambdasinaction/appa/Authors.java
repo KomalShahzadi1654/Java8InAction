779736ea6f4ae7c42d6a4323ac93e843c0dd2ce6
KomalShahzadi <komal.shahzadi@techlogix.com>
2019-07-25 16:48:49 +0500

    c

--- src/main/java/lambdasinaction/appa/Authors.java
+++ src/main/java/lambdasinaction/appa/Authors.java
@@ -3,9 +3,6 @@ package lambdasinaction.appa;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-
-//comment
-//2nd commit
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authors {
 
